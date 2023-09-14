0db7fa95d4a4372e6248a3cb948da617956ec09f
Chris Eidhof <chris@eidhof.nl>
2023-09-14 10:38:33 +0200

    Changes

--- Sources/AttributedStringBuilder/AttributedStringBuilder.swift
+++ Sources/AttributedStringBuilder/AttributedStringBuilder.swift
@@ -10,6 +10,28 @@ struct AttributedStringBuilder {
     public static func buildOptional<C: AttributedStringConvertible>(_ component: C?) -> some AttributedStringConvertible {
         component.map { [$0] } ?? []
     }
+
+    public static func buildEither<L, R>(first component: L) -> Either<L, R> {
+        Either.l(component)
+    }
+
+    public static func buildEither<L, R>(second component: R) -> Either<L, R> {
+        Either.r(component)
+    }
+}
+
+public enum Either<A, B> {
+    case l(A)
+    case r(B)
+}
+
+extension Either: AttributedStringConvertible where A: AttributedStringConvertible, B: AttributedStringConvertible {
+    public func attributedString(context: inout Context) -> [NSAttributedString] {
+        switch self {
+        case let .l(l): return l.attributedString(context: &context)
+        case let .r(r): return r.attributedString(context: &context)
+        }
+    }
 }
 
 extension AttributedStringConvertible {
